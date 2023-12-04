3348007ea4c36657aad55e5ae4ea191d237e11fa
Abiksoumya <soumyamohanty540@gmail.com>
2023-12-04 11:01:58 +0530

    auth setup

---
+++ index.ts
@@ -0,0 +1,26 @@
+
+import express, { NextFunction, Request, Response } from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import { AppError } from "./utils/appError";
+import { errorHandler } from "./utils/errorHandler";
+config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+
+// app.use("/api/v1/auth", authRouter);
+
+app.all("*", (req: Request, _res: Response, next: NextFunction) => {
+  next(new AppError(`Can't find ${req.originalUrl} path on the server`, 404));
+});
+
+app.use(errorHandler);
+
+
