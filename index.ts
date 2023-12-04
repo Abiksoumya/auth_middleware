e223ef21d71898469c987f8f1d994a882ba75570
Abiksoumya <soumyamohanty540@gmail.com>
2023-12-04 12:05:00 +0530

    import error resolve

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
-import { AppError } from "./utils/appError";
 import { errorHandler } from "./utils/errorHandler";
 import { Protect } from "./middleware/auth.middleware";
 config();
@@ -19,9 +18,9 @@ app.use(morgan("dev"));
 
 // app.use("/api/v1/auth", authRouter);
 
-app.all("*", (req: Request, _res: Response, next: NextFunction) => {
-  next(new AppError(`Can't find ${req.originalUrl} path on the server`, 404));
-});
+// app.all("*", (req: Request, _res: Response, next: NextFunction) => {
+//   next(new AppError(`Can't find ${req.originalUrl} path on the server`, 404));
+// });
 
 app.use(errorHandler);
 
