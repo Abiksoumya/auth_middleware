8ef6cfe6da91e209cd3359e6a5038b70a0589b40
Abiksoumya <soumyamohanty540@gmail.com>
2023-12-04 12:35:21 +0530

    index js  file update

--- index.ts
+++ index.ts
@@ -1,29 +1,3 @@
-
-import express from "express";
-import { config } from "dotenv";
-import cors from "cors";
-import morgan from "morgan";
-import { errorHandler } from "./utils/errorHandler";
 import { Protect } from "./middleware/auth.middleware";
-config();
 
-
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use(morgan("dev"));
-
-
-// app.use("/api/v1/auth", authRouter);
-
-// app.all("*", (req: Request, _res: Response, next: NextFunction) => {
-//   next(new AppError(`Can't find ${req.originalUrl} path on the server`, 404));
-// });
-
-app.use(errorHandler);
-
-export {Protect}
-
-
+export { Protect };
